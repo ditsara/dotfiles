3bc62b09550c3afc59044e912b626a0bb7d6be54
Dan Itsara <dan@glazziq.com>
2018-09-27 12:55:50 +0700

    fixes

---
+++ install-scripts/3-config-home.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+\rsync -avI home/ ~
--- install-scripts/4-docker.sh
+++ install-scripts/4-docker.sh
@@ -3,7 +3,7 @@
 DISTRO=$(lsb_release -as | head -1 | awk '{print tolower($0)}')
 CODENAME=$(lsb_release -cs)
 
-sudo apt-get install \
+sudo apt-get -y install \
      apt-transport-https \
      ca-certificates \
      curl \
@@ -16,3 +16,4 @@ curl -fsSL https://download.docker.com/linux/$DISTRO/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/$DISTRO $CODENAME stable"
 
+sudo apt-get update && sudo apt-get install -y docker-ce
