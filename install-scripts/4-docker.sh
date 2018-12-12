63d79ea74855a0f143ba677e7ddd71a3bd1748e4
Dan Itsara <dan@glazziq.com>
2018-12-12 21:34:32 +0700

    docker compose

--- install-scripts/4-docker.sh
+++ install-scripts/4-docker.sh
@@ -17,3 +17,6 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/$DISTRO $CODENAME stable"
 
 sudo apt-get update && sudo apt-get install -y docker-ce
+
+sudo curl -L --fail https://github.com/docker/compose/releases/download/1.23.1/run.sh -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
