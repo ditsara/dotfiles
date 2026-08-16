b013cff089bd3e3a0eab3a8be6cbcfa0879a0226
Dan Itsara <dan.itsara@gmail.com>
2026-08-16 12:08:15 +0700

    neovim convert tabs to spaces

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,7 +4,9 @@
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.tabstop = 2
+vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
+vim.o.expandtab = true
 vim.o.smartcase = true
 vim.o.ignorecase = true
 vim.o.wrap = false
